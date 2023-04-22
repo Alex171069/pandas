bb63566bca45e1876b7f534a2bf49253a9b663e7
Alex171069 <alex171069@mail.ru>
2023-04-22 23:53:20 +0700

    Оформил class

--- net.py
+++ net.py
@@ -2,7 +2,17 @@ import pandas
 import openpyxl
 
 class xlsparse:
+    
+ def __init__(self, fileXls, sheet, ucols):
+     self.fileXls = fileXls   # файл .xls для обработки 
+     self.sheet = sheet       # лист который читается 
+     self.ucols = ucols       # взять перечисленные калонки строкой
+     
+ def pars(self):    
+  xlsFile_sheet = pandas.read_excel(self.fileXls, sheet_name=self.sheet, usecols=self.ucols) # лист гораздо больше
+  FileDataF = xlsFile_sheet.head()
+  print(FileDataF)
 
- xlsFile_sheet = pandas.read_excel('07_04_2023.xlsx', sheet_name='Лист1') # лист гораздо больше
- print(xlsFile_sheet)
 
+Xfile=xlsparse('07_04_2023.xlsx','Лист1','A:G')
+Xfile.pars()
