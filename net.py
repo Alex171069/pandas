32c88a36006ce92851bb9dbe2afa89a4c4c2f530
Alex171069 <alex171069@mail.ru>
2023-04-22 17:30:14 +0700

    Добавили класс

--- net.py
+++ net.py
@@ -1,8 +1,8 @@
 import pandas
 import openpyxl
 
+class xlsparse:
 
-xlsFile_sheet = pandas.read_excel('07_04_2023.xlsx', sheet_name='Лист1') # лист гораздо больше
-
-print(xlsFile_sheet)
+ xlsFile_sheet = pandas.read_excel('07_04_2023.xlsx', sheet_name='Лист1') # лист гораздо больше
+ print(xlsFile_sheet)
 
