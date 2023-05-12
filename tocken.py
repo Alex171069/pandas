8b5cc1f9bdc09fc44a88548df0a4b5175605cb78
Alex171069 <alex171069@mail.ru>
2023-05-12 23:33:51 +0700

    Добавил получение данных от Yandex-a

---
+++ tocken.py
@@ -0,0 +1,4 @@
+from yadirstat import yadirstat
+
+query_tok=yadirstat.query_report('y0_AgAAAAANfuiCAAnllgAAAADjBoYPiffY9bEzQRKtYK5bdLwDEk5ZX6Y', 'ya.alex171069', '2023-05-12','2023-05-12')
+print(query_tok)
