39f43b4b91287323ae86e08814b62025e9670e3e
Alex171069 <alex171069@mail.ru>
2023-05-21 07:06:34 +0700

    new commit

--- tocken.py
+++ tocken.py
@@ -1,4 +1,14 @@
-from yadirstat import yadirstat
+from pytrends.request import TrendReq
+import seaborn
+# for styling
+seaborn.set_style("darkgrid")
 
-query_tok=yadirstat.query_report('y0_AgAAAAANfuiCAAnllgAAAADjBoYPiffY9bEzQRKtYK5bdLwDEk5ZX6Y', 'ya.alex171069', '2023-05-12','2023-05-12')
-print(query_tok)
+kw = "python" 
+pt = TrendReq(hl="en-US", tz=360)   # en-US
+# pt.build_payload([kw], timeframe="all")
+# rt = pt.related_topics()
+# rq = pt.related_queries()
+# rq = pt.suggestions("python")
+rq = pt.get_historical_interest(kw, year_start=2018, month_start=1, day_start=1, hour_start=0, year_end=2023, month_end=2, day_end=1, hour_end=0, cat=0, geo='', gprop='', sleep=0)
+print (rq)
+# print(rq[kw]["top"])
